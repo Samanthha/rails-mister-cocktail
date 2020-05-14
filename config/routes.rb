587b32d3d8aad08dc1e51f866aa9f9824383f1c0
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-14 16:09:57 -0400

    all views in place for cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
   get '/cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
+  post '/cocktails/', to: 'cocktails#create'
+
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
 end
