9008cfa35f12e83eeefe94f0d64ef273eba58173
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-14 16:44:13 -0400

    adding cocktail action fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/new', to: 'cocktails#new', as: :new_cocktail
-  get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
-  post '/cocktails/', to: 'cocktails#create'
-
   resources :cocktails do
-    resources :doses, only: [ :new, :create ]
+    resources :doses, only: [ :new, :create, :delete ]
   end
 end
